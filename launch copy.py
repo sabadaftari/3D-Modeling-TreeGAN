ed3c53b875eaba008e8f63cf8264983567fe6994
Saba Daftari <sabadaftari@gmail.com>
2024-10-01 09:12:02 -0700

    clean up

--- launch copy.py
+++ launch copy.py
@@ -82,25 +82,6 @@ class TreeGANDiscriminator(nn.Module):
     def forward(self, x):
         """ Passes the input point cloud `x` through the discriminator network to get the real/fake score. """
         return self.features(x).squeeze()
-        # shape = x.shape[0]
-        # if shape>= 60000:
-        #     # Assuming your discriminator is defined as self.discriminator
-        #     batch_size = 1024  # Define an appropriate batch size
-        #     # Process in batches
-        #     x_flat = x.view(-1, 3) 
-        #     outputs = [self.features(x_flat[i:i + batch_size]) for i in range(0, shape, batch_size)]  # List to store outputs from each batch
-
-        #     # Process in batches
-        #     # for i in range(0, x.shape[0], batch_size):
-        #     #     batch = x.view(-1, 3)[i:i + batch_size]
-        #     #     output = self.features(batch)  # Get output for each batch
-        #     #     outputs.append(output)  # Append the output to the list
-
-        #     # Concatenate all outputs into a single tensor
-        #     all_outputs = torch.cat(outputs, dim=0)
-        #     return all_outputs.squeeze()
-        # else:
-        #     return self.features(x).squeeze()
 
 # TreeGCN class to define the architecture of Tree-based Graph Neural Network
 class TreeGCN(nn.Module):
@@ -273,16 +254,10 @@ def gradient_penalty(discriminator, real_data, fake_data, lambda_gp=10):
     Returns:
         gradient_penalty (torch.Tensor): The computed gradient penalty.
     """
-    # alpha = torch.rand(real_data.size(0), 1, 1).to(real_data.device)  # Random scalar for interpolation
-    # interpolates = real_data + alpha * (fake_data - real_data)
-
-    # Define the desired fraction for sampling
-    # Define the desired fraction for sampling
-    sampling_fraction = 0.05
 
     # Calculate the number of samples to use
     total_samples = real_data.size(0)  # Total number of samples in real_data
-    # num_samples = int(total_samples * sampling_fraction)
+
     num_samples = 1024
     # Randomly sample indices
     random_indices = torch.randperm(total_samples)[:num_samples]
@@ -299,17 +274,6 @@ def gradient_penalty(discriminator, real_data, fake_data, lambda_gp=10):
 
 
     interpolates.requires_grad_(True)
-
-    # x_flat = interpolates.view(-1, 3) 
-    # dataset = TensorDataset(x_flat)  # Create a dataset
-    # dataloader = DL(dataset, batch_size=1024, shuffle=False, num_workers=6) 
-    # outputs = []
-    # for batch in dataloader:
-    #     batch_data = batch[0]  # Get the batch data
-    #     output = discriminator(batch_data)  # Process the batch
-    #     outputs.append(output)
-    # # outputs = [discriminator(batch_data[0]) for batch_data in dataloader]
-    # d_interpolates = torch.cat(outputs, dim=0)
     d_interpolates = discriminator(interpolates.view(-1, 3))
     
     gradients = torch.autograd.grad(
